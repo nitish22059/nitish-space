ff50f79cce9f1a8128473ef58e29a019fb0b0d18
P NITISH <ugs22059_cic.nitish@cbit.org.in>
2025-11-05 09:00:51 +0000

    add project

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -123,7 +123,7 @@ const HeroSection = () => {
                     <iframe
                     
                       // src="https://drive.google.com/file/d/1T4bGWUAiWyS8kwFLvdqrI_UFi2c3L7Fp/preview"
-                      src="https://drive.google.com/file/d/1nf9Gx4q5lx-kUvcev87C8oDl6A8xovOH/view?usp=sharing"
+                      src="https://drive.google.com/file/d/1nf9Gx4q5lx-kUvcev87C8oDl6A8xovOH/preview"
                       title="Resume"
                       className="w-full h-[70vh] rounded border shadow-inner bg-gray-50 dark:bg-gray-800"
                       allowFullScreen
